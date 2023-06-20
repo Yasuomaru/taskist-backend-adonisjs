5a2b26eb7357608b9818cdb1c9608cb457c65429
Akira <miguel.silva.422@gmail.com>
2023-06-20 19:00:41 +0100

    v1

--- database/seeders/Task.ts
+++ database/seeders/Task.ts
@@ -1,5 +1,5 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import TaskFactory from 'Database/factories/TaskFactory'
+import TaskFactory from '../factories/TaskFactory'
 
 export default class extends BaseSeeder {
   public async run () {
